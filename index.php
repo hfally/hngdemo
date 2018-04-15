ad0882941e7af62dcc96032a47ed41a0f63f5891
Henry Falade <tofex4eva@yahoo.com>
2018-04-15 23:43:47 +0100

    Initial commit

---
+++ index.php
@@ -0,0 +1,22 @@
+<html>
+
+    <head>
+        <title>HNG Demo</title>
+
+        <link rel="stylesheet" href="main.css" />
+    </head>
+
+    <body>
+        <main>
+            <div class="container">
+                <div class="content">
+                    Hello Henry Falade, the time is
+                    <br />
+                    <i>
+                        <?php echo date('g:i a'); ?>
+                    </i>
+                </div>
+            </div>
+        </main>
+    </body>
+</html>
